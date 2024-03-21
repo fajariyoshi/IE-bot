f5efa3f6b6103d2607f5e9ec7cc9c34a0e9e9e8b
fajariyoshi <56659434+fajariyoshi@users.noreply.github.com>
2024-03-21 17:26:56 +0700

    1
    go

---
+++ smart-enhancer.py
@@ -0,0 +1,111 @@
+"""
+Author: Bisnu Ray
+GitHub: https://github.com/bisnuray/
+Description: A Telegram bot to enhance photos using the Remini API.
+"""
+
+import os
+import base64
+import hashlib
+import httpx
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+bot = Bot(token='6394932970:AAHWG2YYpZrVy3XMwLG_6AlRoiJkQk1jAjg')   # REPLACE YOUR TELEGRAM BOT TOKEN HERE
+dp = Dispatcher(bot)
+
+API_KEY = "vwYDa72zHVgoTpANuB1TfsAQuBq1H5LP2QB3w8RELyxgCfNw"   # REPLACE YOUR REMINI API KEY THAT HAS CREDITS
+CONTENT_TYPE = "image/jpeg"
+_TIMEOUT = 60
+_BASE_URL = "https://developer.remini.ai/api"
+
+def _get_image_md5_content(file_path: str) -> tuple[str, bytes]:
+    with open(file_path, "rb") as fp:
+        content = fp.read()
+        image_md5 = base64.b64encode(hashlib.md5(content).digest()).decode("utf-8")
+    return image_md5, content
+
+async def enhance_photo_and_send_link(file_path: str, chat_id: int):
+    image_md5, content = _get_image_md5_content(file_path)
+
+    async with httpx.AsyncClient(
+        base_url=_BASE_URL,
+        headers={"Authorization": f"Bearer {API_KEY}"},
+    ) as client:
+        response = await client.post(
+            "/tasks",
+            json={
+                "tools": [
+                    {"type": "face_enhance", "mode": "beautify"},
+                    {"type": "background_enhance", "mode": "base"}
+                ],
+                "image_md5": image_md5,
+                "image_content_type": CONTENT_TYPE
+            }
+        )
+        assert response.status_code == 200
+        body = response.json()
+        task_id = body["task_id"]
+
+        response = await client.put(
+            body["upload_url"],
+            headers=body["upload_headers"],
+            content=content,
+            timeout=_TIMEOUT
+        )
+        assert response.status_code == 200
+
+        response = await client.post(f"/tasks/{task_id}/process")
+        assert response.status_code == 202
+
+        for i in range(50):
+            response = await client.get(f"/tasks/{task_id}")
+            assert response.status_code == 200
+
+            if response.json()["status"] == "completed":
+                break
+            else:
+                await asyncio.sleep(2)  # Use asyncio.sleep() instead of sleep()
+
+        output_url = response.json()["result"]["output_url"]
+        await bot.send_message(chat_id, f"<b>Enhanced photo: </b> {output_url}", parse_mode='html')
+
+    # Remove the downloaded photo file
+    os.remove(file_path)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    # Create the inline keyboard and buttons
+    keyboard = InlineKeyboardMarkup(row_width=2)
+    dev_button = InlineKeyboardButton("Oleh", url="https://t.me/pesonagoodboy")
+    update_button = InlineKeyboardButton("Update ✅", url="https://t.me/pesonagoodboy")
+    keyboard.add(dev_button, update_button)
+
+    # Send the welcome message with the inline keyboard
+    await bot.send_message(
+        message.chat.id,
+        "<b>Selamat datang! Saya adalah BOT Penambah Cerdas. Tolong kirimkan saya foto untuk menyempurnakannya.</b>",
+        parse_mode='html',
+        reply_markup=keyboard
+    )
+
+@dp.message_handler(content_types=types.ContentType.PHOTO)
+async def handle_photo(message: types.Message):
+    photo = message.photo[-1]
+    file_path = os.path.join(os.getcwd(), f"{photo.file_unique_id}.jpg")
+    await photo.download(file_path)
+    await bot.send_message(message.chat.id, "<b>Enhancing your photo...</b>", parse_mode='html')
+
+    # Call the enhancement function and pass the file path and chat ID
+    await enhance_photo_and_send_link(file_path, message.chat.id)
+
+@dp.message_handler()
+async def handle_invalid_message(message: types.Message):
+    await bot.send_message(message.chat.id, "<b>Saya tidak diperbolehkan menerima pesan teks atau emoji apa pun.\n\nPlis kirim hanya poto.</b>", parse_mode='html')
+
+# Start the bot
+if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
+    loop.create_task(dp.start_polling())
+    loop.run_forever()
